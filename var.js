11adc9a23caa8c4b5e4354ad97ebafeffe47a29c
Mayank Singh <91192266+mayank1809@users.noreply.github.com>
2024-07-04 11:06:38 +0000

    container save chnges

---
+++ var.js
@@ -0,0 +1,7 @@
+const value1=10;  // fixed for whole code
+let values =10; // can be chnge for the entire code
+var val= 10;    // overriding problem advance feature is let
+
+console.log(val);
+console.log(values);
+console.log(value1);
