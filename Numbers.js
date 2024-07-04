71db706273dd03b7195be079589f1907b1047938
Mayank Singh <91192266+mayank1809@users.noreply.github.com>
2024-07-04 12:08:06 +0000

    number and math

---
+++ Numbers.js
@@ -0,0 +1,35 @@
+// const score = 400
+// console.log(score);
+
+// const bal = 13.4424
+// console.log(bal)
+// console.log(typeof bal);
+// console.log(bal.toString().length);
+// console.log(typeof bal.toFixed(2));
+// console.log(bal.toFixed(2));
+// console.log(bal.toPrecision(3));
+
+// const num = 100000000
+// console.log(num.toLocaleString());
+// console.log(num.toLocaleString('en-IN'));
+
+//////MATHS/////
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(8.9));
+console.log(Math.ceil(8.9));
+console.log(Math.floor(8.9));
+console.log(Math.min(1,2,3,4,54));
+console.log(Math.max(1,2,3,4,54));
+
+console.log(Math.random()) ; // val will be between 0 to 1
+console.log((Math.random()*10) + 1)
+console.log((Math.random()*100 )+ 1) 
+
+const a=10
+const b=21
+
+console.log(Math.max(a,b));
+console.log(Math.min(a,b));
+
