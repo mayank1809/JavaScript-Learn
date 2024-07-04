d2e0def4ae14784448eafd40c102ab29295be79a
Mayank Singh <91192266+mayank1809@users.noreply.github.com>
2024-07-04 11:19:19 +0000

    data types

---
+++ datatype.js
@@ -0,0 +1,45 @@
+/*
+in javascript we dont need to define variable 
+
+primitive (7)
+1- String
+2- undefined
+3- Number
+4- Boolean
+5- null
+6- Symbol (unique)
+7- BigInt (big values)
+
+non primitive (3)
+1- Object       (syntax {} )
+2- Arrays       (syntax [] )
+3- functions    (syntax function functionname(){ } )
+
+
+*/
+
+let name = "mayank"
+let age = 21
+let company;
+let status = true;
+let backlog = null;
+let regnumber = Symbol('12107929')
+let codelines = 4397469326407326432974n;
+
+myobj = {
+    name : "Mayank Singh",
+    age : 21,
+    Year : "3rd"
+
+}
+
+arrayname = [1,2,3,4,5,6]
+
+console.table([name,age,company,status,backlog,regnumber,codelines]);
+
+console.log(myobj);
+
+console.log(arrayname);
+
+//this is the topic for datatype
+
