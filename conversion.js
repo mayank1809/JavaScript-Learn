a04b3cc704faf8f97f4e470db16d7e1b4a8e70dc
Mayank Singh <91192266+mayank1809@users.noreply.github.com>
2024-07-04 11:23:23 +0000

    type conversion and typeof

---
+++ conversion.js
@@ -0,0 +1,12 @@
+let c= "hello"
+let d=123
+let e="123"
+
+console.log(Number(c));  // this will give NaN
+console.log(Number(d));  // this will give 123
+console.log(Number(e));  // this will give 123
+
+let a=null; // type of null is object
+console.log(typeof a);
+
+console.log(Number(a));
